8491b972ac5aa4f262d03b02495596c56c7c77e5
Scarpon <chris.scarpone@gmail.com>
2023-09-30 10:45:57 -0400

    fixed the raster statistics plotting, stream lined that pipeline into one function. Updated the readme. Continue working on the plotting button for the maps.

--- fmt/app.R
+++ fmt/app.R
@@ -1,29 +1,13 @@
-# # Source the required scripts from local directory
+# Source the required scripts from local directory
 
-# source("fmt/global.R")
-# source("r/functions.R")
-# source("fmt/server.R")
-# source("r/pc_obj.R")
-
-
-# # Define the UI and server
-# ui <- source("fmt/ui.R", local = TRUE)$value
-# server <- source("fmt/server.R", local = TRUE)$value
-
-
-# Source the required scripts from Shiny Server directory
-source("global.R")
+source("fmt/global.R")
 source("r/functions.R")
-source("server.R")
+source("fmt/server.R")
 source("r/pc_obj.R")
 
-
 # Define the UI and server
-ui <- source("ui.R", local = TRUE)$value
-server <- source("server.R", local = TRUE)$value
-
-
-
+ui <- source("fmt/ui.R", local = TRUE)$value
+server <- source("fmt/server.R", local = TRUE)$value
 
 # Run the app
 shinyApp(ui = ui, server = server)
