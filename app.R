926de63d1dcf52cbbaddfc23d19da5717bcaa161
Scarpon <chris.scarpone@gmail.com>
2024-06-25 16:22:58 -0400

    removed the FMT/fmt/ directory and fixed the directory pathing

---
+++ app.R
@@ -0,0 +1,14 @@
+# Run this script to start the FMT
+
+source("global.R")
+source("server.R")
+source("r/functions.R")
+source("r/spatial_container.R")
+source("r/meta_obj.R")
+
+# Define the UI and server
+ui <- source("ui.R", local = TRUE)$value
+server <- source("server.R", local = TRUE)$value
+
+# Run the app
+shinyApp(ui = ui, server = server)
