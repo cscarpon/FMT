38d019d6f2b19756d42a2f619ba451f0962bcd2f
Scarpon <chris.scarpone@gmail.com>
2024-09-26 13:31:13 -0400

    cant remember what is on this branch

--- r/Orthos.R
+++ r/Orthos.R
@@ -3,6 +3,26 @@ source("r/functions.R")
 source("r/spatial_container.R")
 source("r/meta_obj.R")
 
+library(mapview)
+library(mapedit)
+
+
+# Plot the DTM or Orthoimage to interactively select GCPs
+mapview(ortho_15_mask)
+
+# Use mapedit to interactively select boulders as points
+gcp_points <- editMap(mapview(dtm))
+
+install.packages("future.apply")
+library(future.apply)
+# Set the plan to use 10 cores
+future::plan(multisession, workers = 10)
+
+install.packages("mapedit")
+
+# Set the number of cores to use (e.g., 4 cores)
+terraOptions(threads = 10)
+
 dir <- "./data/"
 mo_dir <- mo$new(dir)
 print(mo_dir$metadata)
@@ -55,7 +75,109 @@ wms_url <- "https://gis.toronto.ca/arcgis/services/basemap/cot_ortho_2015_color_
 # Layer name you want to scrape (you can find this from the GetCapabilities document)
 layer_name <- "0"  # This is usually the default layer index
 
+ttp_bound <- vect("G:/Thesis/TTP/Data/TTP_Boundary.shp", crs = "EPSG:4326")
+ttp_bound <- terra::project(ttp_bound, "EPSG:2019")
 
+ortho_15_dir <- "D:/Data/Thesis/TTP/City of Toronto Orthoimagery - 2015 - TIFF"
+
+ortho_15_file <- list.files(ortho_15_dir, pattern = ".tif",  full.names = TRUE)
+
+ortho_15_list <- list()
+
+# Function to process each raster
+process_raster <- function(file, ttp_bound) {
+  # Load the raster
+  rast <- rast(file)
+  
+  # Convert raster extent to polygon
+  rast_extent <- terra::as.polygons(ext(rast))
+  terra::crs(rast_extent) <- terra::crs(ttp_bound)
+  
+  # Convert SpatVector to sf objects for intersection
+  rast_extent_sf <- sf::st_as_sf(rast_extent)
+  ttp_bound_sf <- sf::st_as_sf(ttp_bound)
+  
+  # Perform intersection
+  int_bound <- sf::st_intersection(rast_extent_sf, ttp_bound_sf)
+  
+  # Check if int_bound has any features
+  if (nrow(int_bound) > 0) {
+    print("Features found, returning file path")
+    return(file)
+  } else {
+    print("No features found, returning NULL")
+    return(NULL)
+  }
+}
+
+ortho_15_list <- list()
+
+for (i in 1:length(ortho_15_file)) {
+  ortho_15_list[[i]] <- process_raster(ortho_15_file[i], ttp_bound)
+}
+
+# Remove NULL values from the list
+valid_files <- ortho_15_list[!sapply(ortho_15_list, is.null)]
+
+new_dir <- "D:/Data/Thesis/TTP/TTP_2015"
+
+# Move files to the new directory
+for (file in valid_files) {
+  file.copy(file, new_dir, overwrite = TRUE)
+  file.remove(file)  # Optionally delete the original file
+}
+
+ttp_15_list <- list.files(new_dir, pattern = ".tif", full.names = TRUE)
+
+ttp_PenC <- lapply(ttp_15_list, rast)
+
+ortho_PenC15 <- do.call(terra::merge, ttp_PenC)
+
+ortho_Penc15_mask <- terra::mask(ortho_PenC15, ttp_bound)
+
+writeRaster(ortho_Penc15_mask, "D:/Data/Thesis/TTP/TTP_2015/ortho_Penc15_mask.tif")
+
+
+plot(ortho_Penc15_mask)
+
+
+# 2020 Orthos
+
+ttp_2020_dir <- "D:/Data/Thesis/TTP/City of Toronto Orthoimagery - 2020 - TIFF"
+
+ttp_2020_list <- list.files(ttp_2020_dir, pattern = ".tif", full.names = TRUE)
+
+ortho_20_list <- list()
+
+for (i in 1:length(ttp_2020_list)) {
+  ortho_20_list[[i]] <- process_raster(ttp_2020_list[i], ttp_bound)
+}
+
+# Remove NULL values from the list
+valid_files_20 <- ortho_20_list[!sapply(ortho_20_list, is.null)]
+
+new_dir <- "D:/Data/Thesis/TTP/TTP_2019"
+
+# Move files to the new directory
+for (file in valid_files_20) {
+  file.copy(file, new_dir, overwrite = TRUE)
+  file.remove(file)  # Optionally delete the original file
+}
+
+new_dir <- "D:/Data/Thesis/TTP/TTP_2020"
+
+ttp_20_list <- list.files(new_dir, pattern = ".tif", full.names = TRUE)
+
+ttp_PenC_20 <- lapply(ttp_20_list, rast)
+
+ortho_PenC20 <- do.call(terra::merge, ttp_PenC_20)
+ortho_PenC20 <- terra::project(ortho_PenC20, "EPSG:2019")
+
+ortho_PenC20
+
+ortho_Penc20_mask <- terra::mask(ortho_PenC20, ttp_bound)
+
+writeRaster(ortho_Penc20_mask, "D:/Data/Thesis/TTP/TTP_2020/ortho_Penc20_mask.tif")
 
 # Original bounding box
 xmin <- -79.3449481800000029
